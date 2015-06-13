c8fd84e8726d1a7496b1165adba0cf6a959e9601
cryptolemming <aliayoub22@gmail.com>
2015-06-13 03:08:01 -0400

    info move

--- build/.module-cache/a5de6681f440b78bed6fde51f273d7958bdd7988.js
+++ build/.module-cache/a5de6681f440b78bed6fde51f273d7958bdd7988.js
@@ -2,38 +2,38 @@
 
 'use strict';
 
-var Card = React.createClass({
+var Card = React.createClass({displayName: "Card",
 	onClickHandler: function(evt) {
 		return this.props.onClick(this.props.cardIndex)
 	},
 	render: function() {
 		var styling = this.props.styling(this.props.image, this.props.flipped);
 		return (
-			<li className='game-card' onClick={this.onClickHandler} style={styling}></li>
+			React.createElement("li", {className: "game-card", onClick: this.onClickHandler, style: styling})
 		);
 	}
 });
 
-var StartGame = React.createClass({
+var StartGame = React.createClass({displayName: "StartGame",
 	render: function() {
 		var styling = this.props.styling(this.props.gameOver);
 		return (
-			<li className='refresh'><button className='start-button' onClick={this.props.onClick} style={styling}><i className='fa fa-refresh'></i></button></li>
+			React.createElement("li", {className: "refresh"}, React.createElement("button", {className: "start-button", onClick: this.props.onClick, style: styling}, React.createElement("i", {className: "fa fa-refresh"})))
 		);
 	}
 });
 
-var Win = React.createClass({
+var Win = React.createClass({displayName: "Win",
 	render: function() {
 		return (
-			<div className='win-modal'>
-				<p className='win'>YOU WIN!</p>
-		  	</div>
+			React.createElement("div", {className: "win-modal"}, 
+				React.createElement("p", {className: "win"}, "YOU WIN!")
+		  	)
 		);
 	}
 });
 
-var GameBoard = React.createClass({
+var GameBoard = React.createClass({displayName: "GameBoard",
 	shuffleImages: function(array) {
 		var newArray = [];
 		for (var i = 0; i < 6; i += 1) {
@@ -64,14 +64,14 @@ var GameBoard = React.createClass({
 		} else {
 			gameOver = false;
 		}
-		return <StartGame onClick={this.onStartClick} styling={this.gameOverStyling} gameOver={gameOver} />;
+		return React.createElement(StartGame, {onClick: this.onStartClick, styling: this.gameOverStyling, gameOver: gameOver});
 	},
 	boardCreation: function (images, flipped) {
 	    images = images || this.state.imagesArray;
 	    flipped = flipped || this.state.flipped;
 	    var self = this;
 	    return images.map(function (image, i) {
-	         return <Card key={i} onClick={self.onCardFlip} image={image} styling={self.boardStyling} image={image} flipped={flipped[i]} cardIndex={i} />;
+	         return React.createElement(Card, {key: i, onClick: self.onCardFlip, image: image, styling: self.boardStyling, image: image, flipped: flipped[i], cardIndex: i});
 	    });
 	},
 	onStartClick: function() {
@@ -133,7 +133,7 @@ var GameBoard = React.createClass({
 	},
 	won: function(win) {
 		if (win) {
-			return <Win />;
+			return React.createElement(Win, null);
 		}
 	},
 	getInitialState: function() {
@@ -156,30 +156,30 @@ var GameBoard = React.createClass({
 	    };
 	},
 	render: function() {
-		return <div className='game-container'>
-				<div className='game-info'>
-					<ul className='game-counters'>
-						<li><img className='x-counter' src='src/images/redx.png' /></li>
-						<li>{this.state.wrongFlips}</li>
-						<li><img className='walle-counter' src='src/images/walle.jpg' /></li>
-						<li>{this.state.walleCount}/2</li>
-						{this.state.gameInfo}
-				</ul>
-				</div>
-				<div className='game-board'>
-					{this.state.won}
-				  	<ul className='grid'>{this.state.board}</ul>
-				</div>
-				<div className='info'>
-				  <a href='http://aliayoub.com'>
-				    <p className='about' target='_blank'>created by Ali Ayoub</p>
-				  </a>
-				  <p className='copyright'>All artwork and the name Wall-E are property of 
-				    <a href='http://www.pixar.com/' className='pixar'> Pixar</a>
-				  </p>
-				</div>
-			  </div>;
+		return React.createElement("div", {className: "game-container"}, 
+				React.createElement("div", {className: "game-info"}, 
+					React.createElement("ul", {className: "game-counters"}, 
+						React.createElement("li", null, React.createElement("img", {className: "x-counter", src: "src/images/redx.png"})), 
+						React.createElement("li", null, this.state.wrongFlips), 
+						React.createElement("li", null, React.createElement("img", {className: "walle-counter", src: "src/images/walle.jpg"})), 
+						React.createElement("li", null, this.state.walleCount, "/2"), 
+						this.state.gameInfo
+				)
+				), 
+				React.createElement("div", {className: "game-board"}, 
+					this.state.won, 
+				  	React.createElement("ul", {className: "grid"}, this.state.board)
+				), 
+				React.createElement("div", {className: "info"}, 
+				  React.createElement("a", {href: "http://aliayoub.com"}, 
+				    React.createElement("p", {className: "about", target: "_blank"}, "created by Ali Ayoub")
+				  ), 
+				  React.createElement("p", {className: "copyright"}, "All artwork and the name Wall-E are property of",  
+				    React.createElement("a", {href: "http://www.pixar.com/", class: "pixar"}, " Pixar")
+				  )
+				)
+			  );
 	}
 });
 
-React.render(<GameBoard />, document.getElementById('container'));
+React.render(React.createElement(GameBoard, null), document.getElementById('container'));
