ad452744a78373045401e34bd211ce1ba55eb529
cryptolemming <aliayoub22@gmail.com>
2015-06-13 21:26:25 -0400

    dropbox pics and general cleaning

--- build/.module-cache/96b107f4d13f542329ad3d3be6d418f7c018dc53.js
+++ build/.module-cache/96b107f4d13f542329ad3d3be6d418f7c018dc53.js
@@ -4,47 +4,42 @@
 
 // individual cards
 
-var Card = React.createClass({
+var Card = React.createClass({displayName: "Card",
 	onClickHandler: function(evt) {
 		return this.props.onClick(this.props.cardIndex)
 	},
 	render: function() {
 		var styling = this.props.styling(this.props.image, this.props.flipped);
 		return (
-			<li className='game-card' onClick={this.onClickHandler} style={styling}></li>
+			React.createElement("li", {className: "game-card", onClick: this.onClickHandler, style: styling})
 		);
 	}
 });
 
 // refresh button
 
-var StartGame = React.createClass({
+var StartGame = React.createClass({displayName: "StartGame",
 	render: function() {
 		var styling = this.props.styling(this.props.gameOver);
 		return (
-			<li className='refresh'><button className='start-button' onClick={this.props.onClick} style={styling}><i className='fa fa-refresh'></i></button></li>
+			React.createElement("li", {className: "refresh"}, React.createElement("button", {className: "start-button", onClick: this.props.onClick, style: styling}, React.createElement("i", {className: "fa fa-refresh"})))
 		);
 	}
 });
 
 // you win modal
 
-var Win = React.createClass({
+var Win = React.createClass({displayName: "Win",
 	render: function() {
 		return (
-			<div className='win-modal'>
-				<p className='win'>YOU WIN!</p>
-		  	</div>
+			React.createElement("div", {className: "win-modal"}, 
+				React.createElement("p", {className: "win"}, "YOU WIN!")
+		  	)
 		);
 	}
 });
 
-// parent component - game
-
-var Game = React.createClass({
-
-	// shuffle the card images
-
+var Game = React.createClass({displayName: "Game",
 	shuffleImages: function(array) {
 		var newArray = [];
 		for (var i = 0; i < 6; i += 1) {
@@ -52,9 +47,6 @@ var Game = React.createClass({
 		}
 		return newArray;
 	},
-
-	// styling to display the card image when flipped
-
 	boardStyling: function(image, flipped) {
 		if (flipped == true) {
 			return {
@@ -63,9 +55,6 @@ var Game = React.createClass({
 			};
 		}
 	},
-
-	// styling to change color of refresh button when game is over
-
 	gameOverStyling: function(gameOver) {
 		gameOver = this.state.gameOver;
 		if (gameOver) {
@@ -75,31 +64,22 @@ var Game = React.createClass({
 			};
 		}
 	},
-
-	// calling the refresh button component on game over
-
 	infoCreation: function(gameOver) {
 		if (this.state !== null) {
 			gameOver = this.state.gameOver;
 		} else {
 			gameOver = false;
 		}
-		return <StartGame onClick={this.onStartClick} styling={this.gameOverStyling} gameOver={gameOver} />;
+		return React.createElement(StartGame, {onClick: this.onStartClick, styling: this.gameOverStyling, gameOver: gameOver});
 	},
-
-	// calling the card component and passing it the shuffled images to create the game board
-
 	boardCreation: function (images, flipped) {
 	    images = images || this.state.imagesArray;
 	    flipped = flipped || this.state.flipped;
 	    var self = this;
 	    return images.map(function (image, i) {
-	         return <Card key={i} onClick={self.onCardFlip} image={image} styling={self.boardStyling} image={image} flipped={flipped[i]} cardIndex={i} />;
+	         return React.createElement(Card, {key: i, onClick: self.onCardFlip, image: image, styling: self.boardStyling, image: image, flipped: flipped[i], cardIndex: i});
 	    });
 	},
-
-	// changing state when refresh button clicked
-
 	onStartClick: function() {
 		var images = this.shuffleImages(['k3xkgdci3h9mlnf/walle.jpg?dl=0', 'k3xkgdci3h9mlnf/walle.jpg?dl=0', '1ll4rd0q28y7is8/eve.jpg?dl=0', '7sbiokkeq2hnaze/john.jpg?dl=0', '93ltebnju2vd5ns/captain2.jpg?dl=0', 'uho6nbflui260ca/mary.jpg?dl=0']);
 	    var flipped = [false, false, false, false, false, false];
@@ -119,26 +99,14 @@ var Game = React.createClass({
 	        board: this.boardCreation(images, flipped, win),
 	    });
 	},
-
-	// changing state and declaring behavior on different game over scenarios when a card is flipped
-
 	onCardFlip: function(cardIndex) {
-
-		// if game is over do not allow more card flipping
-
 		if (this.state.gameOver) {
 			return ;
 		}
-
-		// temporary values to work with state
-
 		var currentStateFlipped = this.state.flipped;
 		currentStateFlipped[cardIndex] = true;
 		var currentImagesFlipped = this.state.imagesFlipped;
 		currentImagesFlipped[cardIndex] = this.state.imagesArray[cardIndex];
-
-		// counting flips for walles, not walles, and general
-
 		var walles = currentImagesFlipped.reduce(function(n, value) {
 			return value == 'k3xkgdci3h9mlnf/walle.jpg?dl=0' ? n + 1 : n;
 		}, 0);
@@ -149,18 +117,12 @@ var Game = React.createClass({
 			return value !== undefined ? n + 1 : n;
 		}, 0);
 		var updatedBoard = this.boardCreation(this.state.imagesArray, currentStateFlipped);
-
-		// win scenario - 2 walles flipped
-
 		if (walles >= 2) {
 			this.setState({
 				win: true,
 				won: this.won(true),
 				gameOver: true,
 			})
-
-		// other game over scenarios
-
 		} else if ((walles == 1 && flips == 5) || (walles == 0 && flips == 4)) {
 			this.setState ({
 				gameOver: true,
@@ -175,15 +137,11 @@ var Game = React.createClass({
 			board: updatedBoard,
 		})
 	},
-
-	// calling win component to render modal
-
 	won: function(win) {
 		if (win) {
-			return <Win />;
+			return React.createElement(Win, null);
 		}
 	},
-
 	getInitialState: function() {
 	    var images = this.shuffleImages(['k3xkgdci3h9mlnf/walle.jpg?dl=0', 'k3xkgdci3h9mlnf/walle.jpg?dl=0', '1ll4rd0q28y7is8/eve.jpg?dl=0', '7sbiokkeq2hnaze/john.jpg?dl=0', '93ltebnju2vd5ns/captain2.jpg?dl=0', 'uho6nbflui260ca/mary.jpg?dl=0']);
 	    var flipped = [false, false, false, false, false, false];
@@ -203,32 +161,31 @@ var Game = React.createClass({
 	        board: this.boardCreation(images, flipped),
 	    };
 	},
-	
 	render: function() {
-		return <div className='game-container'>
-				<div className='game-info'>
-					<ul className='game-counters'>
-						<li><img className='x-counter' src='src/images/redx.png' /></li>
-						<li>{this.state.wrongFlips}</li>
-						<li><img className='walle-counter' src='https://dl.dropboxusercontent.com/s/k3xkgdci3h9mlnf/walle.jpg?dl=0' /></li>
-						<li>{this.state.walleCount}/2</li>
-						{this.state.gameInfo}
-				</ul>
-				</div>
-				<div className='game-board'>
-					{this.state.won}
-				  	<ul className='grid'>{this.state.board}</ul>
-				</div>
-				<div className='info'>
-				  <a href='http://aliayoub.com'>
-				    <p className='about' target='_blank'>created by Ali Ayoub</p>
-				  </a>
-				  <p className='copyright'>All artwork and the name Wall-E are property of 
-				    <a href='http://www.pixar.com/' className='pixar'> Pixar</a>
-				  </p>
-				</div>
-			  </div>;
+		return React.createElement("div", {className: "game-container"}, 
+				React.createElement("div", {className: "game-info"}, 
+					React.createElement("ul", {className: "game-counters"}, 
+						React.createElement("li", null, React.createElement("img", {className: "x-counter", src: "src/images/redx.png"})), 
+						React.createElement("li", null, this.state.wrongFlips), 
+						React.createElement("li", null, React.createElement("img", {className: "walle-counter", src: "https://dl.dropboxusercontent.com/s/k3xkgdci3h9mlnf/walle.jpg?dl=0"})), 
+						React.createElement("li", null, this.state.walleCount, "/2"), 
+						this.state.gameInfo
+				)
+				), 
+				React.createElement("div", {className: "game-board"}, 
+					this.state.won, 
+				  	React.createElement("ul", {className: "grid"}, this.state.board)
+				), 
+				React.createElement("div", {className: "info"}, 
+				  React.createElement("a", {href: "http://aliayoub.com"}, 
+				    React.createElement("p", {className: "about", target: "_blank"}, "created by Ali Ayoub")
+				  ), 
+				  React.createElement("p", {className: "copyright"}, "All artwork and the name Wall-E are property of",  
+				    React.createElement("a", {href: "http://www.pixar.com/", className: "pixar"}, " Pixar")
+				  )
+				)
+			  );
 	}
 });
 
-React.render(<Game />, document.getElementById('container'));
+React.render(React.createElement(Game, null), document.getElementById('container'));
